1366f12f087744067c1bbed64dce14725e723d1e
guozhipeng <zhipeng.guo@bosyun.com>
2018-01-25 15:30:30 +0800

    初始文件

---
+++ app/src/main/java/com/icerain/www/listviewtest/Fruit.java
@@ -0,0 +1,20 @@
+package com.icerain.www.listviewtest;
+
+/**
+ * Created by gzp007 on 2017/11/21.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getImageId() {
+        return imageId;
+    }
+}
---
+++ app/src/main/java/com/icerain/www/listviewtest/ViewHolder.java
@@ -0,0 +1,13 @@
+package com.icerain.www.listviewtest;
+
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by gzp007 on 2017/11/21.
+ */
+
+class ViewHolder {
+    ImageView fruitImage;
+    TextView fruitName;
+}
